dc4c9d23bd444abf4fd57f54ee51db2ad7b1c09e
mahdisrenia-coder <mahdis.rezaeiniasar@gmail.com>
2026-07-22 18:15:51 +0200

    addede the etth2. scripts and modified them--- by mahdis

--- scripts/long_term_forecast/ETT_script/DLinear_ETTh1.sh
+++ scripts/long_term_forecast/ETT_script/DLinear_ETTh1.sh
@@ -24,38 +24,37 @@ python -u run.py \
   --des 'Exp' \
   --itr 1 \
   --batch_size 16 \
-  --train_epochs 3 \
+  --train_epochs 5 \
   --num_workers 0 \
   --no_use_gpu
 
 
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_192 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --batch_size 16 \
+  --train_epochs 5 \
+  --num_workers 0 \
+  --no_use_gpu
+
 #COMMENTED OUT OTHER HORIZONS
-
-# python -u run.py \
-#   --task_name long_term_forecast \
-#   --is_training 1 \
-#   --root_path ./dataset/ETT-small/ \
-#   --data_path ETTh1.csv \
-#   --model_id ETTh1_96_192 \
-#   --model $model_name \
-#   --data ETTh1 \
-#   --features M \
-#   --seq_len 96 \
-#   --label_len 48 \
-#   --pred_len 192 \
-#   --e_layers 2 \
-#   --d_layers 1 \
-#   --factor 3 \
-#   --enc_in 7 \
-#   --dec_in 7 \
-#   --c_out 7 \
-#   --des 'Exp' \
-#   --itr 1 \
-#   --batch_size 16 \
-#   --train_epochs 3 \
-#   --num_workers 0 \
-#   --no_use_gpu
-
 
 # python -u run.py \
 #   --task_name long_term_forecast \
@@ -78,7 +77,7 @@ python -u run.py \
 #   --des 'Exp' \
 #   --itr 1 \
 #   --batch_size 16 \
-#   --train_epochs 3 \
+#   --train_epochs 5 \
 #   --num_workers 0 \
 #   --no_use_gpu
 
@@ -104,6 +103,6 @@ python -u run.py \
 #   --des 'Exp' \
 #   --itr 1 \
 #   --batch_size 16 \
-#   --train_epochs 3 \
+#   --train_epochs 5 \
 #   --num_workers 0 \
 #   --no_use_gpu
--- scripts/long_term_forecast/ETT_script/LightTS_ETTh1.sh
+++ scripts/long_term_forecast/ETT_script/LightTS_ETTh1.sh
@@ -2,7 +2,6 @@
 
 model_name=LightTS
 
-# ONLY RUNNIG PRED_LEN=96
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
@@ -24,36 +23,37 @@ python -u run.py \
   --des 'Exp' \
   --itr 1 \
   --batch_size 16 \
-  --train_epochs 3 \
+  --train_epochs 5 \
+  --num_workers 0 \
+  --no_use_gpu
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_192 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --batch_size 16 \
+  --train_epochs 5 \
   --num_workers 0 \
   --no_use_gpu
 
 # COMMENTED OUT OTHER HORIZIONS
-
-# python -u run.py \
-#   --task_name long_term_forecast \
-#   --is_training 1 \
-#   --root_path ./dataset/ETT-small/ \
-#   --data_path ETTh1.csv \
-#   --model_id ETTh1_96_192 \
-#   --model $model_name \
-#   --data ETTh1 \
-#   --features M \
-#   --seq_len 96 \
-#   --label_len 48 \
-#   --pred_len 192 \
-#   --e_layers 2 \
-#   --d_layers 1 \
-#   --factor 3 \
-#   --enc_in 7 \
-#   --dec_in 7 \
-#   --c_out 7 \
-#   --des 'Exp' \
-#   --itr 1 \
-#   --batch_size 16 \
-#   --train_epochs 3 \
-#   --num_workers 0 \
-#   --no_use_gpu
 
 # python -u run.py \
 #   --task_name long_term_forecast \
@@ -76,7 +76,7 @@ python -u run.py \
 #   --des 'Exp' \
 #   --itr 1 \
 #   --batch_size 16 \
-#   --train_epochs 3 \
+#   --train_epochs 5 \
 #   --num_workers 0 \
 #   --no_use_gpu
 
@@ -101,6 +101,6 @@ python -u run.py \
 #   --des 'Exp' \
 #   --itr 1 \
 #   --batch_size 16 \
-#   --train_epochs 3 \
+#   --train_epochs 5 \
 #   --num_workers 0 \
 #   --no_use_gpu
